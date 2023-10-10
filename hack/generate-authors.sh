501bfaad4c9ab83b30fb428372a809333f578d11
ChrisAdongo <jakenyachris@gmail.com>
2023-10-10 04:19:32 -0700

    generate-authors.sh

--- hack/generate-authors.sh
+++ hack/generate-authors.sh
@@ -3,5 +3,7 @@
 # The root of the Git repository
 cd "$(git rev-parse --show-toplevel)" || exit 1
 
+> AUTHORS
+
 # Use Git log to extract author names and email addresses
 git log --format='%aN <%aE>' | sort -u
